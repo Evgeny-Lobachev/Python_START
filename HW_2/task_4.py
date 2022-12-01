553be0387724f165299aff4a7bd8fd916e239002
Lobachev Evgeniy <e.lobachev@bk.ru>
2022-12-01 18:45:30 +0400

    task4

--- HW_2/task_4.py
+++ HW_2/task_4.py
@@ -6,4 +6,15 @@
 Ввод: значение типа <int>
 Вывод: значение типа <int>
 """
+import random
 n = int(input())
+list = [random.randint(- n, n) for r in range(n)]
+index_l = []
+product = 1
+with open('HW_2\indexes.txt', encoding='UTF-8') as data:
+    for index in data:
+        index_l.append(int(index))
+for i in range(-n, n):
+    if i in index_l:
+        product *= list[i]
+print(product)
