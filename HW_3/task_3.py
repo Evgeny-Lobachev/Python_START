b34a89bb9241f19a0c1e91ed8964b942370f72e5
Lobachev Evgeniy <e.lobachev@bk.ru>
2022-12-04 20:50:09 +0400

    task3

--- HW_3/task_3.py
+++ HW_3/task_3.py
@@ -9,3 +9,13 @@
 [1.1, 1.2, 3.1, 5, 10.01]
 2.0
 """
+import random
+n = int(input())
+my_list = [round(random.uniform(1, 5), 2) for i in range(n)]
+second_list = []
+
+for i in range(len(my_list)):
+    second_list.append(my_list[i] - int(my_list[i]))
+result = max(second_list) - min(second_list)
+print(my_list)
+print(round(result, 2))
