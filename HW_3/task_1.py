606b721eb65a959a5f2636d460cb780e9aeb9b79
Lobachev Evgeniy <e.lobachev@bk.ru>
2022-12-04 20:22:41 +0400

    task1

--- HW_3/task_1.py
+++ HW_3/task_1.py
@@ -11,3 +11,11 @@
 [5, 1, 5, 2, 7, 11]
 14
 """
+import random
+n = int(input())
+my_list = [random.randint(0,n) for i in range(n)]
+print(my_list)
+sum = 0
+for i in range(1, n, 2):
+    sum += my_list[i]
+print(sum)
